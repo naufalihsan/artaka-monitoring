37a248b561bd948972a12973a8d13eee6f92e436
gunturbudikurniawan <gunturbudikurniawan16@gmail.com@users.noreply.github.com>
2021-01-26 10:37:39 +0700

    perbaikan kondisi show all referral code

--- api/models/Sales1.go
+++ api/models/Sales1.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/json"
+	"strings"
 	"time"
 
 	"github.com/jinzhu/gorm"
@@ -42,8 +43,9 @@ func ShowReferralCode(db *gorm.DB) (error, []Referral) {
 		return err, nil
 	}
 	var res []Referral
+
 	for i := 0; i < len(datas); i++ {
-		if datas[i].Referral_code != "" && datas[i].Referral_code != datas[i+1].Referral_code {
+		if datas[i].Referral_code != "" && strings.Contains(strings.ToUpper(datas[i].Referral_code), strings.ToUpper(datas[i+1].Referral_code)) {
 			res = append(res, datas[i])
 		}
 	}
